0bb52e1e74eccf646f8d77469c3f03ceccb30606
fahmiyonda007 <yondadf04@gmail.com>
2023-01-03 11:19:38 +0700

    init

---
+++ src/auth/dto/sign-up-input.dto.ts
@@ -0,0 +1,32 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsAlphanumeric, IsAscii, IsEmail, IsPhoneNumber, MinLength } from 'class-validator';
+
+import { User } from '../../users/users.entity';
+
+@InputType()
+export class SignUpInput implements Partial<User> {
+  @ApiProperty()
+  @Field()
+  @IsAlphanumeric()
+  @MinLength(1)
+  readonly name: string;
+
+  @ApiProperty()
+  @Field()
+  @IsEmail()
+  @MinLength(1)
+  readonly email: string;
+
+  @ApiProperty()
+  @Field()
+  @IsPhoneNumber('ID')
+  @MinLength(1)
+  readonly phone: string;
+
+  @ApiProperty()
+  @Field()
+  @IsAscii()
+  @MinLength(8)
+  readonly password: string;
+}
---
+++ src/users/users.entity.ts
@@ -0,0 +1,53 @@
+import { Field, ID, Int, ObjectType } from '@nestjs/graphql';
+import { Exclude } from 'class-transformer';
+import { IsPhoneNumber } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  IsNull,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  VersionColumn,
+} from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class User {
+  @Field((type) => ID)
+  @PrimaryGeneratedColumn()
+  readonly id: number;
+
+  @Field()
+  @Index({ unique: true })
+  @Column()
+  name: string;
+
+  @Field()
+  @Index({ unique: true })
+  @Column()
+  email: string;
+
+  @Exclude({ toPlainOnly: true })
+  @Column()
+  password: string;
+
+  @Field()
+  @Index({ unique: true })
+  @IsPhoneNumber('ID')
+  @Column({nullable: true})
+  phone: string;
+
+  @Field()
+  @CreateDateColumn()
+  readonly createdAt: Date;
+
+  @Field()
+  @UpdateDateColumn()
+  readonly updatedAt: Date;
+
+  @Field((type) => Int)
+  @VersionColumn()
+  readonly version: number;
+}
